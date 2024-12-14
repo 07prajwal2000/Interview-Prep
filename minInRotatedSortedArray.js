7ca6440397b4475371610918078d5420c974e960
Prajwal Aradhya <manuaradhya@gmail.com>
2024-12-14 00:07:17 +0000

    commented code

--- minInRotatedSortedArray.js
+++ minInRotatedSortedArray.js
@@ -1,25 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var findMin = function (nums) {
   let l = 0, r = nums.length - 1;
-  if (nums[l] <= nums[r]) return nums[l];
-  while (l < r) {
-    const mid = (l + r) >> 1;
-    if (nums[mid] < nums[mid - 1]) {
+  if (nums[l] < nums[r]) return nums[l]; // no rotation
+  while (l <= r) {
+    let mid = (l + r) >> 1;
+    if (nums[mid - 1] > nums[mid]) { // mid's previous is greater, i.e. min is mid
       return nums[mid];
     } else if (nums[mid + 1] < nums[mid]) {
+      // mid is greater than mid's next, i.e. mid + 1 is min
       return nums[mid + 1];
     }
-    if (nums[mid] >= nums[l]) {
+    if (nums[mid + 1] > nums[l]) { // rotated
       l = mid + 1;
-    } else {
+    } else { // not rotated
       r = mid - 1;
     }
   }
   return nums[l];
 };
 
-console.log(findMin([3,4,5,1,2])); // 1
-console.log(findMin([11,13,15,17])); // 11
+console.log(findMin([3, 4, 5, 1, 2])); // 1
+console.log(findMin([11, 13, 15, 17])); // 11
