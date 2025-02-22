9f7eca3acb98b3ce4682eb9d5853967b8a47453f
Prajwal Aradhya <manuaradhya@gmail.com>
2025-02-22 00:14:39 +0000

    added 1 problem

--- problems/nextGreaterElement I.js
+++ problems/nextGreaterElement I.js
@@ -3,6 +3,40 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+var nextGreaterElementSoln2 = function (nums1, nums2) {
+  const m = nums2.length;
+  let n = nums1.length;
+  let s = [], nge = Array(m), map = {};
+
+  for (let i = m - 1; i >= 0; i--) {
+    let cur = nums2[i];
+    map[cur] = i;
+
+    if (s.length == 0) {
+      nge[i] = -1;
+    } else {
+      while (s.length && cur > (s[s.length - 1] ?? -1e9)) {
+        s.pop();
+      }
+      nge[i] = s[s.length - 1] ?? -1;
+    }
+
+    s.push(cur);
+  }
+
+  let ans = [];
+
+  for (let num of nums1) {
+    ans.push(nge[map[num]]);
+  }
+  return ans;
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
 var nextGreaterElement = function (nums1, nums2) {
   const m = nums2.length;
   let n = nums1.length;
