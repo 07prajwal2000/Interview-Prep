126a31f08005187363891fee379c680ae06a0b25
Prajwal Aradhya <manuaradhya@gmail.com>
2025-04-09 20:20:35 +0100

    tabulation added

--- problems/dp/burstBalloons.js
+++ problems/dp/burstBalloons.js
@@ -4,26 +4,34 @@
  */
 var maxCoins = function (nums) {
   if (nums.length == 1) return nums[0];
-  nums = [1, ...nums, 1];
+  nums.unshift(1);
+  nums.push(1);
   const n = nums.length;
-  const dp = Array(n).fill(0).map(x => Array(n).fill(-1));
+  const dp = Array(n);
+  for (let i = 0; i < n; i++) {
+    dp[i] = Array(n).fill(0);
+  }
+  for (let start = n - 2; start >= 1; start--) {
+    for (let end = start; end < n - 1; end++) {
+      let maxCoins = 0;
+      for (let i = start; i <= end; i++) {
+        let curCoins = nums[start - 1] * nums[i] * nums[end + 1];
 
+        curCoins += dp[start][i - 1];
+        curCoins += dp[i + 1][end];
+
+        maxCoins = Math.max(maxCoins, curCoins);
+      }
+      dp[start][end] = maxCoins;
+    }
+  }
+  return dp[1][n - 2];
   function find(start, end) {
     if (start > end) return 0;
-    if (start == end) {
-      let curCoins = nums[start];
-      if (start - 1 >= 0) curCoins *= nums[start - 1];
-      if (end + 1 < n) curCoins *= nums[end + 1];
-      return curCoins;
-    }
     if (dp[start][end] != -1) return dp[start][end];
     let maxCoins = 0;
     for (let i = start; i <= end; i++) {
-      let curCoins = nums[i];
-
-      if (start - 1 >= 0) curCoins *= nums[start - 1];
-      if (end + 1 < n) curCoins *= nums[end + 1];
-
+      let curCoins = nums[start - 1] * nums[i] * nums[end + 1];
       curCoins += find(start, i - 1);
       curCoins += find(i + 1, end);
 
@@ -34,3 +42,5 @@ var maxCoins = function (nums) {
 
   return find(1, n - 2);
 };
+
+console.log(maxCoins([3, 1, 5, 8]));
