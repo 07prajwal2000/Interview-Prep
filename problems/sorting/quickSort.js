631c89158486b258d8d2dfb448daf79b0af23a7d
Prajwal Aradhya <manuaradhya07@gmail.com>
2025-04-21 22:09:26 +0100

    modified quicksort

--- problems/sorting/quickSort.js
+++ problems/sorting/quickSort.js
@@ -1,4 +1,4 @@
-function quickSort(arr) {
+function quickSortUsingSpace(arr) {
   const n = arr.length;
   if (n <= 1) return arr;
   const pivot = arr[n - 1];
@@ -12,9 +12,35 @@ function quickSort(arr) {
       left.push(ele);
     }
   }
-  left = quickSort(left);
-  right = quickSort(right);
+  left = quickSortUsingSpace(left);
+  right = quickSortUsingSpace(right);
   return [...left, pivot, ...right];
 }
 
-console.log(quickSort([6, 5, 4, 3, 2, 1]));
+function quickSort(arr) {
+  const n = arr.length;
+  function sort(low, high) {
+    if (low >= high) return;
+    let pivot = arr[low];
+    let i = low, j = high;
+    while (i < j) {
+      while (i <= high - 1 && arr[i] <= pivot) i++;
+      while (j >= low - 1 && arr[j] > pivot) j--;
+
+      if (j > i) {
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+    let tmp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = tmp;
+    sort(low, j - 1);
+    sort(j + 1, high);
+  }
+  sort(0, n - 1);
+  return arr;
+}
+
+console.log(quickSort([6, 2, 4, 3, 2, 1]));
