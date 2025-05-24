5bfd3aa65466b2888e3230400dcbfc9d8c4449e9
Prajwal Aradhya <manuaradhya07@gmail.com>
2025-05-24 21:55:17 +0100

    1 problem

--- problems/topKFrequent.js
+++ problems/topKFrequent.js
@@ -4,6 +4,22 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
+  const heap = new MaxPriorityQueue(x => x[0]), map = {};
+  for (let num of nums) {
+    map[num] = num in map ? map[num] + 1 : 1;
+  }
+  for (let k in map) {
+    heap.push([map[k], parseInt(k)]);
+  }
+  const ans = [];
+  while (k) {
+    ans.push(heap.pop()[1]);
+    k--;
+  }
+  return ans;
+}
+
+var topKFrequent2 = function (nums, k) {
   const map = {};
   let maxCount = 0;
   for (let num of nums) {
