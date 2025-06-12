9d5907363753ae78ec01696f313e5ce0946dc3a4
Prajwal Aradhya <manuaradhya07@gmail.com>
2025-06-13 00:58:27 +0100

    changed with comments

--- problems/textJustification.js
+++ problems/textJustification.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/text-justification/solutions/6837932/beats-100-javascript-code-with-comments-rp9bv/
 /**
  * @param {string[]} words
  * @param {number} maxWidth
@@ -5,19 +6,21 @@
  */
 var fullJustify = function (words, maxWidth) {
   const ans = [], n = words.length;
+  // to get total characters at the current index of words list
   const wordsCountMap = {};
   let i = 0;
   while (i < n) {
-    let len = 0;
-    const w = [];
+    const w = []; // temp words list
+    let len = 0; // to calculate total chars added to temp list
     let charLength = 0;
     while (i < n) {
       const word = words[i];
-      len += word.length;
-      if (len > maxWidth) break;
+      len += word.length; // add the chars length before to check if it exceeds
+      if (len > maxWidth) break; // if exceeds exit from loop
       len++;
       w.push(word);
       i++;
+      // counting total chars for future reference
       charLength += word.length;
     }
     wordsCountMap[ans.length] = charLength;
@@ -25,24 +28,33 @@ var fullJustify = function (words, maxWidth) {
   }
   const res = [];
   for (let j = 0; j < ans.length - 1; j++) {
-    const list = ans[j];
+    const list = ans[j]; // list of words in jth line
+    // total chars in current line excluding space
     const charsLength = wordsCountMap[j];
+    // total available space to insert
     let availableSpace = maxWidth - charsLength;
+    // total position to insert space
     let m = list.length - 1 == 0 ? 1 : list.length - 1;
-
+    // final sentence
     let sentence = '';
     for (let k = 0; k < list.length; k++) {
       const word = list[k];
       sentence += word;
+      // total space required after inserting the word
       let spaceRequired = Math.ceil(availableSpace / m);
       for (let l = 0; l < spaceRequired; l++) sentence += ' ';
+      // decrement the available space as we inserted the space to sentence
       availableSpace -= spaceRequired;
+      // decrement the total words that are pending to be inserted
       m--;
+      // if goes < 0, will reset back to 1
       m = m == 0 ? 1 : m;
     }
     res.push(sentence);
   }
+  // no complicated calculations for the last line. simple join with space
   let sentence = ans[ans.length - 1].join(' ');
+  // insert the remaining spaces to sentence
   for (let i = sentence.length; i < maxWidth; i++) {
     sentence += ' ';
   }
