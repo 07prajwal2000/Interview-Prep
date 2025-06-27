084d432d297b1c537ac9a0d3c24eb174703f18c1
Prajwal Aradhya <manuaradhya07@gmail.com>
2025-06-27 22:37:37 +0100

    modified 1 problem

--- problems/longestSubsequenceRepeatedKTimes.js
+++ problems/longestSubsequenceRepeatedKTimes.js
@@ -2,8 +2,11 @@ function countSub(s, tmp) {
   const n = s.length, m = tmp.length;
   let i = 0, j = 0, count = 0;
   while (i < n) {
+    // if both char match
     if (s[i] == tmp[j]) {
+      // increment the pointer
       j++;
+      // if j reaches end, reset it and increment the counter
       if (j == m) {
         count++;
         j = 0;
@@ -11,6 +14,7 @@ function countSub(s, tmp) {
     }
     i++;
   }
+  // this gives how many times the tmp word is in `s`.
   return count;
 }
 
@@ -18,16 +22,22 @@ var longestSubsequenceRepeatedK = function (s, k) {
   const freq = {};
   for (let c of s) freq[c] = (freq[c] || 0) + 1;
 
-  const q = [''];
+  const q = ['']; // queue for bfs
   let res = '';
   while (q.length) {
     const cur = q.shift();
+    // looping a -> z
     for (let i = 0; i < 26; i++) {
       const key = String.fromCharCode('a'.charCodeAt(0) + i);
+      // checking if the key exist and count should be greater than k
       if (key in freq && freq[key] >= k) {
+        // generating the str
         let str = cur + key;
+        // checking if this str exist in s k times
         if (countSub(s, str) < k) continue;
+        // if so, we add it to the queue
         q.push(str);
+        // update the result, as it stores in lexicographical order
         res = str;
       }
     }
